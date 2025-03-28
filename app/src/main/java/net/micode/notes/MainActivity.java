1ccd98f70e98af3440ba4132bcba19d50802f9a7
misaka14739 <1429032152@qq.com>
2025-03-28 16:13:02 +0800

    first

---
+++ app/src/main/java/net/micode/notes/MainActivity.java
@@ -0,0 +1,13 @@
+package net.micode.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
